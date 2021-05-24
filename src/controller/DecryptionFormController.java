4266789f655e4a99a60f87e9bab7d9ff70eead6a
Sadisha Eranga <sadishaeranga19678@gmail.com>
2021-05-24 23:02:25 +0530

    Finalize Project

--- src/controller/DecryptionFormController.java
+++ src/controller/DecryptionFormController.java
@@ -1,6 +1,8 @@
 package controller;
 
 import javafx.event.ActionEvent;
+import javafx.scene.control.Alert;
+import javafx.scene.control.ButtonType;
 import javafx.scene.control.TextField;
 
 public class DecryptionFormController {
@@ -9,5 +11,51 @@ public class DecryptionFormController {
     public TextField txtText;
 
     public void btnDecrypt_OnAction(ActionEvent actionEvent) {
+        String cipherText = txtCipherText.getText();
+        String key = txtKey.getText();
+
+        if(cipherText.trim().isEmpty()){
+            new Alert(Alert.AlertType.ERROR, "Please enter valid cipher text", ButtonType.OK).show();
+            txtText.requestFocus();
+            return;
+        }
+
+        if(key.trim().isEmpty()){
+            new Alert(Alert.AlertType.ERROR, "Please enter valid key", ButtonType.OK).show();
+            txtKey.requestFocus();
+            return;
+        }
+
+        //text += key;
+        //String reversedText ="";
+        //for(int i = text.length() -1; i>= 0; i--){
+        //    reversedText += text.charAt(i);
+        //}
+        //
+        //String ciphertext = "";
+        //for(int i=0; i<reversedText.length(); i++){
+        //    int code= reversedText.charAt(i);
+        //    code += 10;
+        //    char newChar = (char) code;
+        //    ciphertext += newChar;
+        //}
+        //
+        //txtCipherText.setText(ciphertext);
+        String reversedText = "";
+        for(int i = 0; i < cipherText.length(); i++){
+            int code = cipherText.charAt(i);
+            code -= 10;
+            char orgChar = (char) code;
+            reversedText += orgChar;
+        }
+
+        String originalText = "";
+        for(int i = reversedText.length() - 1; i >= 0; i--){
+               originalText += reversedText.charAt(i);
+        }
+
+        originalText = originalText.substring(0,originalText.length() - key.length());
+        txtText.setText(originalText);
+
     }
 }
--- src/controller/EncryptionFormController.java
+++ src/controller/EncryptionFormController.java
@@ -1,6 +1,8 @@
 package controller;
 
 import javafx.event.ActionEvent;
+import javafx.scene.control.Alert;
+import javafx.scene.control.ButtonType;
 import javafx.scene.control.TextField;
 
 public class EncryptionFormController {
@@ -9,5 +11,37 @@ public class EncryptionFormController {
     public TextField txtCipher;
 
     public void btnEncrypt_OnAction(ActionEvent actionEvent) {
+        String text = txtText.getText();
+        String key = txtKey.getText();
+
+        if(text.trim().isEmpty()){
+            new Alert(Alert.AlertType.ERROR, "Please enter valid text", ButtonType.OK).show();
+            txtText.requestFocus();
+            return;
+        }
+
+        if(key.trim().isEmpty()){
+            new Alert(Alert.AlertType.ERROR, "Please enter valid key", ButtonType.OK).show();
+            txtKey.requestFocus();
+            return;
+        }
+
+        text += key;
+        String reversedText ="";
+        for(int i = text.length() -1; i>= 0; i--){
+            reversedText += text.charAt(i);
+        }
+
+        String ciphertext = "";
+        for(int i=0; i<reversedText.length(); i++){
+            int code= reversedText.charAt(i);
+            code += 10;
+            char newChar = (char) code;
+            ciphertext += newChar;
+        }
+        txtCipher.setText(ciphertext);
+
     }
+
+
 }
