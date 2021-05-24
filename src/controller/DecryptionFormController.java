e4875672e08b0ea2d51a46e3d68bcdedf27c93e0
Sadisha Eranga <sadishaeranga19678@gmail.com>
2021-05-24 17:28:27 +0530

    Finalize UIs

--- src/controller/DecryptionFormController.java
+++ src/controller/DecryptionFormController.java
@@ -1,4 +1,13 @@
 package controller;
 
+import javafx.event.ActionEvent;
+import javafx.scene.control.TextField;
+
 public class DecryptionFormController {
+    public TextField txtCipherText;
+    public TextField txtKey;
+    public TextField txtText;
+
+    public void btnDecrypt_OnAction(ActionEvent actionEvent) {
+    }
 }
--- src/controller/EncryptionFormController.java
+++ src/controller/EncryptionFormController.java
@@ -1,4 +1,13 @@
 package controller;
 
+import javafx.event.ActionEvent;
+import javafx.scene.control.TextField;
+
 public class EncryptionFormController {
+    public TextField txtText;
+    public TextField txtKey;
+    public TextField txtCipher;
+
+    public void btnEncrypt_OnAction(ActionEvent actionEvent) {
+    }
 }
